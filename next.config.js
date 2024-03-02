8deb5368bdf20dd4f8500f34b37a04fcd9725eec
Syuro <64142347+syurodev@users.noreply.github.com>
2024-03-02 13:34:09 +0700

    feat: PWA application

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})
 
 const nextConfig = {
+  ...withPWA,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
